ae5b1a0e995a0c6d4ac46d5e59f229c1a1b8ccab
amjad <amjad@bluej.org>
2017-05-18 10:32:55 +0100

    test

---
+++ q.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class q here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class q
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class q
+     */
+    public q()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
---
+++ we.java
@@ -0,0 +1,8 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+
+/**
+ * 
+ */
+public abstract class we
+{
+}
