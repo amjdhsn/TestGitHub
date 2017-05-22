a112d71a841afea759f112a95f4e96cfdc341181
amjad <amjad@bluej.org>
2017-05-22 11:43:48 +0100

    g
    g
    g

---
+++ Test2.java
@@ -0,0 +1,15 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+
+/**
+ * 
+ */
+public class Test2
+{
+
+    /**
+     * Constructor for objects of class Test2
+     */
+    public Test2()
+    {
+    }
+}
