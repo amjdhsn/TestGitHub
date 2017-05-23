22d046e610315f704d64abd9d5ae19478d1726fe
amjad <amjad@bluej.org>
2017-05-23 14:10:06 +0100

    test

---
+++ ww.java
@@ -0,0 +1,15 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+
+/**
+ * 
+ */
+public class ww
+{
+
+    /**
+     * Constructor for objects of class ww
+     */
+    public ww()
+    {
+    }
+}
