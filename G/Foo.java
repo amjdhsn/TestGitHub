7dc2f25c733531c169715beb93304f9d2b880ccc
amjad <amjad@bluej.org>
2017-07-24 17:31:33 +0100

    t

---
+++ G/Foo.java
@@ -0,0 +1,35 @@
+package G;
+
+
+/**
+ * Write a description of class Foo here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Foo
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Foo
+     */
+    public Foo()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
