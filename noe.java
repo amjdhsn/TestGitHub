e72fb4f8eea7f9e5c357a0f8f80684b18a9166c6
amjad <amjad@bluej.org>
2017-08-17 10:51:30 +0100

    a

--- noe.java
+++ noe.java
@@ -8,7 +8,7 @@
 public class noe
 {
     // instance variables - replace the example below with your own
-    private int x;
+    private int x; 
 
     /**
      * Constructor for objects of class noe
