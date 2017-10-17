0b28d5d0d60a2972126268a2118292a404dac8b4
amjad <amjad@bluej.org>
2017-10-17 13:55:08 +0100

    1

---
+++ qa/AD.java
@@ -0,0 +1,35 @@
+package qa;
+
+
+/**
+ * Write a description of class AD here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class AD
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class AD
+     */
+    public AD()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
