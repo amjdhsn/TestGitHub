50bd92307fccf3b46f94bf6c624899f0ecb97448
amjad <amjad@bluej.org>
2017-10-24 14:35:47 +0100

    w

---
+++ w.java
@@ -0,0 +1,16 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+import lang.stride.*;
+
+/**
+ * 
+ */
+public class w
+{
+
+    /**
+     * Constructor for objects of class w
+     */
+    public w()
+    {
+    }
+}
